343ab60c8ffab1ac5915f22390fb374ed77374ae
KrishKoria <krishkoria2004@gmail.com>
2023-12-22 00:38:51 +0530

    removed apikey

--- Backend/Util/location.js
+++ Backend/Util/location.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import {HttpErrors} from "../models/http-errors.js";
+import dotenv from 'dotenv';
 
-const API_KEY = 'AIzaSyCtBJWSz-dc5BJjyqE7I4MdgOxCV8lrMpk';
-
+dotenv.config();
 export const getCoords = async (address) => {
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)}&key=${API_KEY}`)
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)}&key=${process.env.API_KEY}`);
     const data = response.data;
     if (!data || data.status === 'ZERO_RESULTS') {
         throw new HttpErrors('Could not find location for the specified address.', 422);
