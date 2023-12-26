2ddf3b0369564a8e63ec7e4321a60be1fa1f61c9
KrishKoria <krishkoria2004@gmail.com>
2023-12-26 18:49:48 +0530

    Added Rudimentary Auto Login And Auto Logout

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,48 @@ import {UserPlaces} from "./places/pages/UserPlaces.jsx";
 import {UpdatePlace} from "./places/pages/UpdatePlace.jsx";
 import {Authenticate} from "./users/pages/Authenticate.jsx";
 import {authContext} from "./shared/components/Util/Context/auth-context.jsx";
-import {useState, useCallback} from "react";
+import {useState, useCallback, useEffect} from "react";
 import React from "react";
+
+let logoutTimer;
 
 const App = () => {
     const [token, setToken] = useState();
     const [userId, setUserId] = useState(false);
-
-    const login = useCallback((uid, token) => {
+    const [tokenExpirationDate, setTokenExpirationDate] = useState(Date);
+    const login = useCallback((uid, token, expirationDate) => {
         setToken(token);
+        const tokenDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+        localStorage.setItem('userData', JSON.stringify({
+            userId: uid,
+            token: token,
+            expiration: tokenDate.toISOString()
+        }))
+        setTokenExpirationDate(tokenDate);
         setUserId(uid)
     }, [])
     const logout = useCallback(() => {
         setToken(null);
+        localStorage.removeItem('userData');
+        setTokenExpirationDate(null);
         setUserId(null)
     }, [])
+
+    useEffect(() => {
+        const storedData = JSON.parse(localStorage.getItem('userData'));
+        if (storedData && storedData.token && new Date(storedData.expiration) > new Date()) {
+            login(storedData.userId, storedData.token, new Date(storedData.expiration));
+        }
+    }, [login]);
+
+    useEffect(() => {
+        if(token && tokenExpirationDate){
+            const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+            logoutTimer = setTimeout(logout, remainingTime)
+        } else {
+            clearTimeout(logoutTimer);
+        }
+    }, [token, logout, tokenExpirationDate])
 
     let routes;
     if (token) {
@@ -44,7 +71,7 @@ const App = () => {
         )
     }
     return (
-        <authContext.Provider value={{isLoggedIn: !!token, token : token, login: login, logout: logout, userId: userId}}>
+        <authContext.Provider value={{isLoggedIn: !!token, token: token, login: login, logout: logout, userId: userId}}>
             <BrowserRouter>
                 <MainNavigation/>
                 <main>
